6c32e97030f2f1e528d2bb41bbe5fbab81f11579
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-03-17 23:54:32 -0700

    modified home page

--- src/main/java/com/githubauto/webdriver/GithubHomePage.java
+++ src/main/java/com/githubauto/webdriver/GithubHomePage.java
@@ -2,6 +2,7 @@ package com.githubauto.webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class GithubHomePage {
   private WebDriver driver;
@@ -9,11 +10,27 @@ public class GithubHomePage {
   public GithubHomePage(WebDriver driver) {
     this.driver = driver;
   }
+  
+  public void load(){
+    driver.navigate().to("https://github.com/");
+  }
+  
+  public GitHubFeaturesPage gotoFeatures(){
+    //click features link
+    driver.findElement(By.linkText("Features")).click();
+    //featuresLink.get
+    //return new page
+    return new GitHubFeaturesPage(driver);
+  }
 
   public GithubLoginPage Signin() {
     driver.get("https://github.com");
     driver.findElement(By.linkText("Sign in")).click();;
     return new GithubLoginPage(driver);
+  }
+  public String getTitle(){
+    return driver.getTitle();
+    
   }
 
 }
